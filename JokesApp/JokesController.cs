4ce217b643a2a7d3ec5e89aecb653fbc9ffb707d
Radko <rkkrowicki@gmail.com>
2022-05-16 23:07:13 +0200

    Start.

---
+++ JokesApp/JokesController.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JokesApp.Models;
+
+namespace JokesApp
+{
+    public class JokesController : Controller
+    {
+        private readonly JokesDataContext _context;
+
+        public JokesController(JokesDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jokes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Jokes != null ? 
+                          View(await _context.Jokes.ToListAsync()) :
+                          Problem("Entity set 'JokesDataContext.Jokes'  is null.");
+        }
+
+        // GET: Jokes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Jokes == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Jokes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            return View(joke);
+        }
+
+        // GET: Jokes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jokes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(joke);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(joke);
+        }
+
+        // GET: Jokes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Jokes == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Jokes.FindAsync(id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+            return View(joke);
+        }
+
+        // POST: Jokes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (id != joke.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(joke);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JokeExists(joke.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(joke);
+        }
+
+        // GET: Jokes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Jokes == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Jokes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            return View(joke);
+        }
+
+        // POST: Jokes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Jokes == null)
+            {
+                return Problem("Entity set 'JokesDataContext.Jokes'  is null.");
+            }
+            var joke = await _context.Jokes.FindAsync(id);
+            if (joke != null)
+            {
+                _context.Jokes.Remove(joke);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JokeExists(int id)
+        {
+          return (_context.Jokes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
